b1203eaafb2644f5e91d16fb641bcfaa47e1b5e8
arrshsh <arsh311202@gmail.com>
2025-05-14 15:47:40 -0400

    Structure edit

---
+++ setup.py
@@ -0,0 +1,11 @@
+# to install local package in virtul file 
+from setuptools import find_packages, setup
+
+setup(
+    name = "mcqgenerator",
+    version= "0.0.1",
+    author= "arrshsh",
+    author_email= "vahora4@uwindsor.ca",
+    install_requires = ["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages= find_packages()
+)
